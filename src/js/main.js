38ec792fccfc6c43d5678403f7044132a31cfb33
mrdokenny <mrdokenny@gmail.com>
2017-01-05 12:20:35 -0800

    Some Tweaks and Google Verification

--- src/js/main.js
+++ src/js/main.js
@@ -77,9 +77,9 @@ BRUSHED.slider = function(){
 		thumb_links				:	0,			// Individual thumb links for each slide
 		thumbnail_navigation    :   0,			// Thumbnail navigation
 		slides 					:  	[			// Slideshow Images
-											{image : 'src/img/slider-images/image01.jpg', title : '<div class="slide-content">Kenny Do</div>', thumb : '', url : ''},
-											{image : 'src/img/slider-images/image02.jpg', title : '<div class="slide-content">Kenny Do</div>', thumb : '', url : ''},
-											{image : 'src/img/slider-images/image03.jpg', title : '<div class="slide-content">Kenny Do</div>', thumb : '', url : ''},
+											{image : 'src/img/slider-images/image01.jpeg', title : '<div class="slide-content">Kenny Do</div>', thumb : '', url : ''},
+											{image : 'src/img/slider-images/image02.png', title : '<div class="slide-content">Kenny Do</div>', thumb : '', url : ''},
+											{image : 'src/img/slider-images/image03.jpeg', title : '<div class="slide-content">Kenny Do</div>', thumb : '', url : ''},
 											{image : 'src/img/slider-images/image04.jpg', title : '<div class="slide-content">Kenny Do</div>', thumb : '', url : ''}  
 									],
 									

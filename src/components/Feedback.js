b5ace444c05957b60b616520115bc50f7fef7499
Kenny Do <contact@kennydo.com>
2018-03-13 15:40:00 -0700

    Move submit button to right

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -4,13 +4,14 @@ import { FormGroup, Checkbox, FormControl, Button } from "react-bootstrap";
 class Feedback extends React.Component {
     state = {
     	text: { value: "", validation: null },
+    	honeyPot: "",
     	isAnon: false,
     	email: "",
     	submitted: false
     }
 
     onSubmitButton() {
-    	if (this.state.text.value.length > 0) {
+    	if (this.state.text.value.length > 0 && this.state.honeyPot === "") {
     		fetch("https://api.kennydo.com/feedback", {
     			method: "POST",
     			headers: {
@@ -46,7 +47,11 @@ class Feedback extends React.Component {
                     } */}
     					<textarea value={this.state.text.value} onChange={(e) => this.setState({ text: { value: e.target.value, validation: e.target.value === "" ? "error" : "success" } })} className="form-control"></textarea>
     				</FormGroup>
-    				<Button style={{ marginTop: "5px" }} type="submit">Submit</Button>
+					<div style={{ display: "none" }}>
+    					<label>Keep this field blank</label>
+						<input value={this.state.honeyPot.value} onChange={(e) => this.setState({ honeyPot: e.target.value })} type="text" />
+    				</div>
+    				<Button style={{ marginTop: "7px", float: "right" }} type="submit">Submit</Button>
 
     			</form>
     			<iframe style={{ display: "none" }} name="frame"></iframe>

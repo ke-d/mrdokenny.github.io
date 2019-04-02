2ee29c8efc4fa394ef0aa4f080d524e5e588da7e
Kenny Do <contact@kennydo.com>
2019-04-02 07:47:45 -0700

    Change backend link

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,13 @@
 // tslint:disable-next-line:import-name
 import ApolloClient from 'apollo-boost';
 import * as React from 'react';
-import {
-  ApolloProvider,
-} from 'react-apollo';
+import { ApolloProvider } from 'react-apollo';
 import * as ReactDOM from 'react-dom';
-import {
-  BrowserRouter,
-} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 const client = new ApolloClient({
-  uri: 'https://api.kennydo.com/graphql',
+  uri: 'https://api.gitmystats.com/graphql',
 });
 
 ReactDOM.render(
